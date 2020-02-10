062f8f2df0fa7fc8f50935712667515c6a57ab2e
Francesinezmaria <56597661+Francesinezmaria@users.noreply.github.com>
2020-02-10 15:15:23 +0000

    Why

--- 4_SVM_and_likelihood/Python/svm_questions_FCR.py
+++ 4_SVM_and_likelihood/Python/svm_questions_FCR.py
@@ -15,23 +15,54 @@ from sklearn.datasets.samples_generator import make_blobs
 
 # consider two classes of points which are well separated
 X, y = make_blobs(n_samples=50, centers=2,
-                  random_state=0, cluster_std=0.50)
+                  random_state=0, cluster_std=0.5)
 plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='autumn')
 
 plt.show()
+
+np.shape(X)
 
 # Task 1: Attempt to use linear regression to separate this data using linear regression.
 # Note there are several possibilities which separate the data? 
 # What happens to the classification of point [0.6, 2.1] (or similar)?
 
 
+xfit = np.linspace(-1, 3.5)
+print(xfit)
+plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='autumn')
+
+for m, b in [(0.8, 0.23), (1.2, 1.5), (-0.5, 3.4)]:
+    yfit = m * xfit + b
+    plt.plot(xfit, yfit, '-k')
+    
+plt.plot([0.6], [2.1], 'x', color = 'red', markeredgewidth = 2, markersize = 10)
+
+plt.xlim(-1, 3.5)
+plt.show()
+
 
 
 # With SVM rather than simply drawing a zero-width line between the 
 # classes, we draw a margin of some width around each line, up to the nearest point. 
-# For example for these lines:
+# For example for these lines (n.b. d is uncertainty around your line):
 
-""" 
+
+xfit = np.linspace(-1, 3.5)
+plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='autumn')
+
+for m, b, d in [(1, 0.65, 0.33), (0.5, 1.6, 0.55), (-0.2, 2.9, 0.2)]: 
+    yfit = m * xfit + b
+    plt.plot(xfit, yfit, '-k')
+    plt.fill_between(xfit, yfit - d, yfit + d, edgecolor='none',
+                     color='#AAAAAA', alpha=0.4)
+
+plt.xlim(-1, 3.5)
+plt.show()
+
+# Task 2: Draw the margin around the lines you chose in Task 1. Use plot. fill between
+#to create confidence intervals (margins refers to area of uncertainty in these lines) by adding
+#and subtracting uncertainty term d from predicted values; 
+
 xfit = np.linspace(-1, 3.5)
 plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='autumn')
 
@@ -40,12 +71,9 @@ for m, b, d in [(1, 0.65, 0.33), (0.5, 1.6, 0.55), (-0.2, 2.9, 0.2)]:
     plt.plot(xfit, yfit, '-k')
     plt.fill_between(xfit, yfit - d, yfit + d, edgecolor='none',
                      color='#AAAAAA', alpha=0.4)
-
+    
 plt.xlim(-1, 3.5)
 plt.show()
-"""
-# Task 2: Draw the margin around the lines you chose in Task 1.
-
 
 
 
